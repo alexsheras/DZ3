6c35bb785259468f3d98a68c5672f887adeb844c
ALEX SHER <sheras85@mail.com>
2022-12-13 19:54:21 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("введите пятизначное число: ");
+int r = Convert.ToInt32(Console.ReadLine());
+if ((r%100000-r%10000)/10000 == r%10 && (r%10000-r%1000)/1000==(r%100-r%10)/10)
+{
+    Console.WriteLine("Полиндром");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
